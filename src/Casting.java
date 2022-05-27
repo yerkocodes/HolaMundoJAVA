d9c52256473bfb81b4da515292ffef88cace3b85
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-05-27 15:40:17 -0400

    Added Casting class

---
+++ src/Casting.java
@@ -0,0 +1,28 @@
+public class Casting {
+    public static void main(String[] args) {
+        // En un anio ubique 30 perritos
+        // Cuantos perritos ubique al mes
+
+        double monthlyDogs = 30.0 / 12.0;
+        System.out.println(monthlyDogs);
+
+        // Estimacion
+        int estimatedMonthlyDogs = (int) monthlyDogs;
+        System.out.println(estimatedMonthlyDogs);
+
+        //Exactitud
+        int a = 30;
+        int b = 12;
+
+        System.out.println((double) a / b);
+        double c = (double) a / b;
+        System.out.println(c);
+
+        char n = '1';
+        int nI = n;
+        System.out.println(nI);
+
+        short nS = (short) n;
+        System.out.println(nS);
+    }
+}
